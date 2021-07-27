04d63bb30a6647d98017278bfe4bdc7e184c8f3f
snaztoz <hafidhmn@gmail.com>
2021-07-27 22:11:52 +0700

    Membuat service database.

--- api/users.js
+++ api/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const database = require('../services/database');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async (req, res, next) => {
+  const conn = await database();
+  const [rows] = await conn.query('SELECT 1 + 1 AS result');
+
+  res.send({result: rows[0].result});
 });
 
 
