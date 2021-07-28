fce9c27b2c4356de1cc982ac9fdea65a6f10af66
snaztoz <hafidhmn@gmail.com>
2021-07-28 13:03:12 +0700

    Mengubah metode akses model.

--- api/users.js
+++ api/users.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const orm = require('../services/orm');
+const { sequelize } = require('../models');
 
 
 router.get('/', async (req, res, next) => {
-  const models = await orm();
-  const users = await models.User.findAll();
+  const users = await sequelize.models.User.findAll();
 
   res.send(JSON.stringify(users, null, 4));
 });
